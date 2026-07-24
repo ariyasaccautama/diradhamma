445bd7de441782a66c626b583499bd50bc4e23d8
Ariya <ariyasacca20@gmail.com>
2026-07-24 18:07:24 +0700

    update rsvp

--- src/components/section/RSVP.tsx
+++ src/components/section/RSVP.tsx
@@ -62,8 +62,8 @@ export default function RSVP({
     useState(guestName);
 
   const [attendance, setAttendance] =
-    useState<AttendanceStatus>(
-      "Hadir"
+    useState<AttendanceStatus | null>(
+      null
     );
 
   const [message, setMessage] =
@@ -97,7 +97,7 @@ export default function RSVP({
     useState("");
 
   const [originalAttendance, setOriginalAttendance] =
-    useState<AttendanceStatus>("Hadir");
+    useState<AttendanceStatus | null>(null);
 
   const [originalMessage, setOriginalMessage] =
     useState("");
@@ -256,6 +256,13 @@ setTotalPages(data.totalPages || 1);
     if (!name.trim()) {
       toast.error(
         "Nama belum terisi"
+      );
+      return;
+    }
+
+    if (!attendance) {
+      toast.error(
+        "Silakan pilih status kehadiran"
       );
       return;
     }
